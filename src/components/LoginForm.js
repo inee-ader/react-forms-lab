3eb9d7bc88fa80f8032c999719a32f2412d4a54a
xoliio <xoliio@gmail.com>
2020-12-14 16:43:48 -0800

    didn't pass all tests, but the app behaves the way it's supposed to

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,23 +3,57 @@ import React from "react";
 class LoginForm extends React.Component {
   constructor() {
     super();
+    this.state = {
+      username: '', 
+      password: ''
+    };
+  }
 
-    this.state = {};
+  handleInputChange = (e) => {
+    // console.log(e.target.value)
+    if(e.target.name == 'username'){
+      this.setState({
+        username: e.target.value
+      })
+    }if(e.target.name == 'password'){
+      this.setState({
+        password: e.target.value
+      })
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const username = this.state.username
+    const password = this.state.password
+    this.props.handleLogin({username, password})
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit} >
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,15 +3,23 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
+    this.state = {
+      value: ''
+    };
+  }
 
-    this.state = {};
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.handleChange} type="text" name="message" id="message" />
+      <p>Characters remaining: {(this.props.maxChars)-(this.state.value.length)}</p>
       </div>
     );
   }
